acdaf4ff9d30ecc8e7f17ecf86102f9f21c1a833
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-09-02 18:42:10 +0200

    WIP

--- main/src/flix/runtime/library/Process.java
+++ main/src/flix/runtime/library/Process.java
@@ -1,5 +1,7 @@
 package flix.runtime.library;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.stream.Collectors;
 
 // TODO: Finish implementation.
@@ -8,15 +10,21 @@ public class Process {
     private long pid;
     private long parent;
     private String command;
+    private String[] arguments;
     private boolean isAlive;
     private String user;
+    private long start;
+    private long cpu;
 
-    public Process(long pid, long parent, String command, boolean isAlive, String user) {
+    public Process(long pid, long parent, String command, boolean isAlive, String user, String arguments[], long start, long cpu) {
         this.pid = pid;
         this.parent = parent;
         this.command = command;
+        this.arguments = arguments;
         this.isAlive = isAlive;
         this.user = user;
+        this.start = start;
+        this.cpu = cpu;
     }
 
     public long getPid() {
@@ -40,6 +48,18 @@ public class Process {
         return user;
     }
 
+    public String[] getArguments() {
+        return arguments;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public long getCpu() {
+        return cpu;
+    }
+
     public static Process[] getProcesses() {
         var handles = ProcessHandle.allProcesses().collect(Collectors.toList());
         var result = new Process[handles.size()];
@@ -48,9 +68,12 @@ public class Process {
             var pid = handle.pid();
             var parent = handle.parent().orElse(handle).pid();
             var command = handle.info().command().orElse("");
+            var arguments = handle.info().arguments().orElse(new String[0]);
             var isAlive = handle.isAlive();
             var user = handle.info().user().orElse("");
-            result[i] = new Process(pid, parent, command, isAlive, user);
+            var start = handle.info().startInstant().orElse(Instant.now()).toEpochMilli();
+            var cpu = handle.info().totalCpuDuration().orElse(Duration.ZERO).toNanos();
+            result[i] = new Process(pid, parent, command, isAlive, user, arguments, start, cpu);
         }
         return result;
     }
