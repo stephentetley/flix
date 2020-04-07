e4874483f2101bb610688e447fec7e526d7e388e
stephentetley <stephen.tetley@gmail.com>
2020-04-07 13:51:50 +0100

    Starting work on Regex modules.

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -106,6 +106,10 @@ class Flix {
 
     "RedBlackTree.flix" -> LocalResource.get("/library/RedBlackTree.flix"),
 
+    "flix/regex/Pattern.flix" -> LocalResource.get("/library/flix/regex/Pattern.flix"),
+    "flix/regex/Matcher.flix" -> LocalResource.get("/library/flix/regex/Matcher.flix"),
+    "flix/regex/MatchResult.flix" -> LocalResource.get("/library/flix/regex/MatchResult.flix"),
+
     //"CodePatterns.flix" -> LocalResource.get("/library/CodePatterns.flix"),
     // "Vec.flix" -> LocalResource.get("/library/Vec.flix"),
 
