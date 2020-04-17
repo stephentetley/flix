4315ffe23ecdda69c2e7fe34e7bcee1a8c9fb366
stephentetley <stephen.tetley@gmail.com>
2020-04-17 11:47:15 +0100

    Added a Java module to allow access to java.util.regex.Pattern flags. Implemented split and compileWithFlags.

---
+++ main/src/flix/runtime/library/Regex.java
@@ -0,0 +1,25 @@
+package flix.runtime.library;
+
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static int getCANON_EQ() { return Pattern.CANON_EQ; }
+
+    public static int getCASE_INSENSITIVE() { return Pattern.CASE_INSENSITIVE; }
+
+    public static int getCOMMENTS() { return Pattern.COMMENTS; }
+
+    public static int getDOTALL() { return Pattern.DOTALL; }
+
+    public static int getLITERAL() { return Pattern.LITERAL; }
+
+    public static int getMULTILINE() { return Pattern.MULTILINE; }
+
+    public static int getUNICODE_CASE() { return Pattern.UNICODE_CASE; }
+
+    public static int getUNICODE_CHARACTER_CLASS() { return Pattern.UNICODE_CHARACTER_CLASS; }
+
+    public static int getUNIX_LINES() { return Pattern.UNIX_LINES; }
+
+}
