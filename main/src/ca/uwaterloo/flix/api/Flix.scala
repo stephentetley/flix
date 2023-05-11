454deb9d8b2c552a8938849bd8d926d1d1d42543
stephentetley <stephen.tetley@gmail.com>
2023-05-11 19:02:45 +0100

    Updated existing Iterator code with the _double thunk_ code from IteratorZ. Updated stdlib to compile with new Iterator typings. Some library tests fail...

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -171,7 +171,6 @@ class Flix {
     "Int64.flix" -> LocalResource.get("/src/library/Int64.flix"),
     "Iterable.flix" -> LocalResource.get("/src/library/Iterable.flix"),
     "Iterator.flix" -> LocalResource.get("/src/library/Iterator.flix"),
-    "IteratorZ.flix" -> LocalResource.get("/src/library/IteratorZ.flix"),
     "List.flix" -> LocalResource.get("/src/library/List.flix"),
     "Map.flix" -> LocalResource.get("/src/library/Map.flix"),
     "Nec.flix" -> LocalResource.get("/src/library/Nec.flix"),
